c604e2e220da70ee55115fcbe1f48ed52ecb136e
Gabriel Cánepa <--global>
2021-12-08 11:28:46 -0300

    Added permissions on snippet views

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.models import User
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
-from rest_framework import generics
+from snippets.permissions import IsOwnerOrReadOnly
+from snippets.serializers import SnippetSerializer, UserSerializer
+from rest_framework import generics, permissions
 
 
 # Create your views here.
@@ -11,6 +13,10 @@ class SnippetList(generics.ListCreateAPIView):
 
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -20,3 +26,23 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+
+# ListAPIView and RetrieveAPIView are two generic read-only views
+class UserList(generics.ListAPIView):
+    """
+    List all users
+    """
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    """
+    Retrieve a user
+    """
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
